e4b525104af9c3c3d283570c301aa9bad0372492
Alsnj20 <marieljara656@gmail.com>
2024-05-17 18:53:35 -0500

    metodo insertBefore en la ayuda de hacer la simulación de una pila

--- lab03/js/ejercicio02.js
+++ lab03/js/ejercicio02.js
@@ -7,15 +7,22 @@ const numbers = document.querySelectorAll('.number');
 const clearAC = document.querySelector('#clear');
 let arrPila = [];
 
+const pilaContent = document.querySelector('.pilaContent');
+
 const inputOperations = document.querySelector('#inputOperations');
 
 equal.addEventListener('click', ()=>{
   let operation = inputOperations.value;
   arrPila.push(inputOperations.value);
+  pilaContent.insertBefore(divItem(operation), pilaContent.firstChild);
   let result = eval(operation);
   arrPila.push(result);
+  pilaContent.insertBefore(divItem(result), pilaContent.firstChild);
 })
 
-clearAC.addEventListener('click', ()=>{
-  console.log(arrPila);
-})
+const divItem = (operation)=>{
+  const div = document.createElement('div');
+  div.classList.add('pilaItem');
+  div.textContent = operation;
+  return div;
+}
