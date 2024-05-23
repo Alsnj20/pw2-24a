7dde15031dec1dc7b36c54e2cd7dd8d8da5833d0
Alsnj20 <marieljara656@gmail.com>
2024-05-23 10:02:55 -0500

    modified

--- lab04/exercises/exercise1/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.end('<h1>Hello World</h1>');
-});
-
-server.listen(3000, () => {
-  console.log('Server is listening en http://localhost:3000');
-});
