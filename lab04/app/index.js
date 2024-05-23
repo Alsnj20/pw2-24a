6a799018d8f64af790cccc5c8d29c0df118ef451
Alsnj20 <mjarama@unsa.edu.pe>
2024-05-23 12:25:18 -0500

    Agenda Personal: Se crea el index.html y se sirve con el servidor

--- lab04/app/index.js
+++ lab04/app/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"))
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server started on http://localhost:3000');
 });
+
+
