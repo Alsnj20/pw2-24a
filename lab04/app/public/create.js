36efa49414704bf7984b270d929c6942eb1a915d
Alsnj20 <marieljara656@gmail.com>
2024-05-25 15:23:55 -0500

    Agenda Personal: Modificando create

--- lab04/app/public/create.js
+++ lab04/app/public/create.js
@@ -1,12 +1,13 @@
 const create = () => {
-  const titleItem = document.getElementById('create').value;
+  const title = document.getElementById('title').value;
+  const description = document.getElementById('description').value;
   const date = document.getElementById('date').value;
-  const title = titleItem.substring(0, 4);
+  //Separar la fecha de la hora
   const parts = date.split('T');
   const datePart = parts[0]; //Fecha
   const timePart = parts[1].replace(':', '-'); //Hora
   if (titleItem && date) {
-    console.log("Titulo: " + title, "Fecha: " + datePart);
+    console.log("Titulo: " + title, "Descripçion: "+description, "Fecha: " + datePart, "Hora: " + timePart);
     //Enviar la informacion al servidor
     const xhr = new XMLHttpRequest();
     xhr.open('POST', '/create', true);
@@ -17,8 +18,7 @@ const create = () => {
         list();
       }
     }
-    console.log((JSON.stringify({ date: datePart, title: title})));
-    xhr.send(JSON.stringify({ date: datePart, title: timePart}));
+    xhr.send(JSON.stringify({title: title, description: description, date: datePart, time: timePart}));
   }
 }
 
