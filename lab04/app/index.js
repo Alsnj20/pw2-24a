4f44b29f3413a0604db629c172e04db460e8d6fe
Alsnj20 <marieljara656@gmail.com>
2024-05-25 10:32:47 -0500

    Agenda Personal: El servidor envia los archivos de forma correcta

--- lab04/app/index.js
+++ lab04/app/index.js
@@ -1,38 +1,45 @@
 const express = require('express');
-const app = express();
 const fs = require('fs');
 const path = require('path');
 
+//Crear una instancia de express
+const app = express();
+
+
+//Cualquier archivo desde la carpeta public sera accedido desdee el navegador
 app.use(express.static("public"))
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
+//Iniciar el servidor
 app.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
 });
 
+//Servidor responde
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/list', (req, res) => {
     console.log('GET /list');
+    //Fecha y titulo del evento
     const data = {
         dates: []
     };
+    //Lectura de directorios
     const folders = fs.readdirSync(path.resolve(__dirname, 'private', 'agenda'));
-    folders.forEach(function (folder) {
-        files = fs.readdirSync(path.resolve(__dirname, 'private', 'agenda', folder))
+    folders.forEach((folder) => {
+        const files = fs.readdirSync(path.resolve(__dirname, 'private', 'agenda', folder))
         const titles = [];
-        files.forEach(function (file) {
-            content = fs.readFileSync(path.resolve(__dirname, 'private', 'agenda', folder, file), 'utf8')
+        files.forEach((file) => {
+            const content = fs.readFileSync(path.resolve(__dirname, 'private', 'agenda', folder, file), 'utf8')
+            //Agregar el titulo del evento
             titles.push(content.substring(0, content.indexOf('\n')));
         });
+        console.log(titles);
         data.dates.push({
             date: folder,
             titles: titles
         });
-        console.log("Entro "+folder);
     });
     console.log(data);
     res.json(data);
