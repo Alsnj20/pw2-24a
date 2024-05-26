1d77e859a766b62bab9043c1f6e7794ac63bbc11
Alsnj20 <marieljara656@gmail.com>
2024-05-25 22:03:13 -0500

    Agenda Personal: Lista de archivos en order jerarquico

--- lab04/app/public/script/listTask.js
+++ lab04/app/public/script/listTask.js
@@ -1,45 +1,15 @@
-/*let editButton;
-let removeButton;
 
-//Cuando la pagina se carge completamente
-onload = () =>{ 
-    initButtons();
-    list();
-}
-
-const initButtons = () => {
-    editButton = document.createElement('button');
-    editButton.innerHTML = 'Editar';
-    editButton.setAttribute('onclick', 'edit(this)');
-
-    removeButton = document.createElement('button');
-    removeButton.innerHTML = 'Eliminar';
-    removeButton.setAttribute('onclick', 'remove(this)');
-}
-
-const createTask = (title) => {
-    const divTask = document.createElement('div');
-    const titleTask = document.createElement('p');
-    const divOptions = document.createElement('div');
-    divOptions.classList.add('options');
-    titleTask.innerHTML = title;
-    divOptions.append(editButton.cloneNode(true), removeButton.cloneNode(true));
-    divTask.append(titleTask, divOptions);
-    return divTask;
-}
-
-const createBlock = (date, titles) => {
-    const divDateTasks = document.createElement('div');
+const createBlock2 = (date, data) => {
+    const divDateTasks = document.createElement('li');
     const divTitles = document.createElement('p');
     const divTasks = document.createElement('ul');
     
-    divTitles.innerHTML = date;
+    divTitles.innerHTML = date+" [DIR]";
 
-    titles.forEach((title) =>{
-        const li = document.createElement('li');
-        const task = createTask(title);
-        li.appendChild(task);
-        divTasks.appendChild(li);
+    data.forEach((file) =>{
+        const task = document.createElement('li');
+        task.innerHTML = file.time+".txt [FILE]";
+        divTasks.appendChild(task);
     });
 
     divDateTasks.append(divTitles, divTasks);
@@ -48,7 +18,7 @@ const createBlock = (date, titles) => {
 
 
 //Obtiene y muestra la lista de desde el servidor
-const list = () => {
+const listTask = () => {
     //Solicitud al servidor HTTP
     const xhr = new XMLHttpRequest();
     //Cuando la solicitud se complete
@@ -58,14 +28,11 @@ const list = () => {
             const data = JSON.parse(xhr.responseText);
             console.log(data.dates);
             data.dates.forEach((day) => {
-                console.log(day.title, day.description, day.time);
-                /*console.log(day.date, day.titles);
-                createBlock(day.date, day.titles);
+                createBlock2(day.date, day.data);
             });
-            console.log(data);
         }
     }
     xhr.open('GET', '/list', true);
     xhr.send();
-}*/
+}
 
