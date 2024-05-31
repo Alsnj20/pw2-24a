4910f637e66a094301bbc6328939a9d762c91ec7
Alsnj20 <marieljara656@gmail.com>
2024-05-31 00:09:57 -0500

    Ejercicio 2g completado

--- lab05/Tarea-del-Ajedrez/Ejercicio2g.py
+++ lab05/Tarea-del-Ajedrez/Ejercicio2g.py
@@ -1,2 +1,16 @@
 from interpreter import draw
 from chessPictures import *
+
+
+tableRow = square.join(square.negative()).horizontalRepeat(4)
+darkPieces = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock).negative()
+darkPawn = pawn.negative().horizontalRepeat(8)
+
+
+rowsPiecesDark = darkPieces.under(tableRow).up(darkPawn.under(tableRow.negative()))
+
+tableSquare = tableRow.up(tableRow.negative()).verticalRepeat(2)
+
+rowPiecesLight = darkPawn.negative().under(tableRow).up(darkPieces.negative().under(tableRow.negative()))
+
+draw(rowsPiecesDark.up(tableSquare).up(rowPiecesLight))
