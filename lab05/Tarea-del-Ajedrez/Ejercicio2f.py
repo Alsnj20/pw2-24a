014547c67fa22c72931cfc4f79e985de4be82d5c
Alsnj20 <marieljara656@gmail.com>
2024-05-30 23:49:35 -0500

    Ejercicio 2f completado

--- lab05/Tarea-del-Ajedrez/Ejercicio2f.py
+++ lab05/Tarea-del-Ajedrez/Ejercicio2f.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+
+
+tableRow1 = square.join(square.negative()).horizontalRepeat(4)
+tableRow2 = tableRow1.negative()
+draw(tableRow1.up(tableRow2).verticalRepeat(2))
