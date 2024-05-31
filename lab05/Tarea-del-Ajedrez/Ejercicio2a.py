b4c1cadc8ef8bea0c8622a0755d707d74459bb56
Alsnj20 <marieljara656@gmail.com>
2024-05-30 23:24:19 -0500

    Ejercicio 2b completado

--- lab05/Tarea-del-Ajedrez/Ejercicio2a.py
+++ lab05/Tarea-del-Ajedrez/Ejercicio2a.py
@@ -2,8 +2,5 @@ from interpreter import draw
 from chessPictures import *
 from picture import Picture
 
-#row1 = knight.join(knight.negative)
-#row2 = (knight.negative).join(knight)
-
 row1 = Picture(KNIGHT).join((Picture(KNIGHT)).negative())
 draw(row1.up(row1.negative()))
--- lab05/Tarea-del-Ajedrez/Ejercicio2b.py
+++ lab05/Tarea-del-Ajedrez/Ejercicio2b.py
@@ -1,2 +1,6 @@
 from interpreter import draw
 from chessPictures import *
+from picture import Picture
+
+row1 = Picture(KNIGHT).join((Picture(KNIGHT)).negative())
+draw(row1.up(row1.horizontalMirror()))
