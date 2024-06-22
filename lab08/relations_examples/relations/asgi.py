56d4ee4de14e031bc2c46f814dc466ab6d079ecb
Alsnj20 <marieljara656@gmail.com>
2024-06-22 18:43:20 -0500

    app examples

--- lab08/relations_examples/relations/asgi.py
+++ lab08/relations_examples/relations/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for relations_examples project.
+ASGI config for relations project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relations_examples.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relations.settings')
 
 application = get_asgi_application()
--- lab08/relations_examples/relations/wsgi.py
+++ lab08/relations_examples/relations/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for relations_examples project.
+WSGI config for relations project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relations_examples.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relations.settings')
 
 application = get_wsgi_application()
