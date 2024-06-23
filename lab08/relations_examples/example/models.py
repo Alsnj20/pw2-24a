541b6702325a3dcf3ff7e6b998a8355aea7e8bc4
Alsnj20 <marieljara656@gmail.com>
2024-06-22 19:29:46 -0500

    Definiendo los modelos Movie y Character

--- lab08/relations_examples/example/models.py
+++ lab08/relations_examples/example/models.py
@@ -13,3 +13,17 @@ class Framework(models.Model):
 
   def __str__(self):
     return self.name
+  
+# Many to Many
+class Movie(models.Model):
+  name = models.CharField(max_length=10)
+  
+  def __str__(self):
+    return self.name
+
+class Character(models.Model):
+  name = models.CharField(max_length=10)
+  movies = models.ManyToManyField(Movie)
+
+  def __str__(self):
+    return self.name
