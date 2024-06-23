4ee447fa27b0f8ba52441138d9e33160ed1cc40b
Alsnj20 <marieljara656@gmail.com>
2024-06-22 23:52:30 -0500

    Se crea la vista para ver y descargar el pdf

--- lab08/print_pdfs/app/views.py
+++ lab08/print_pdfs/app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
+from ..project.renderers import render_to_pdf
 # Create your views here.
+
+
+
+def GeneratePDF(View):
+  def get(self, request, *args, **kwargs):
+    template = get_template('invoice.html')
+    context = {
+        'invoice_id': 123,
+        'customer_name': 'John Cooper',
+        'amount': 1399.99,
+        'today': 'Today',
+    }
+    html = template.render(context)
+    return HttpResponse(html)
