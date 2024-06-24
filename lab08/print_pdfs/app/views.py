7c5ba1779a09c8c02a62fc5d142cf6a48f4ad7c0
Alsnj20 <marieljara656@gmail.com>
2024-06-23 20:48:22 -0500

    pdf download

--- lab08/print_pdfs/app/views.py
+++ lab08/print_pdfs/app/views.py
@@ -14,4 +14,13 @@ class GeneratePDF(View):
     }
     html = template.render(context)
     pdf = render_to_pdf('pdf/invoice.html', context)
+    if pdf:
+      response = HttpResponse(pdf, content_type='application/pdf')
+      filename = "Invoice_%s.pdf" %("121212")
+      content = "inline; filename='%s'" %(filename)
+      response['Content-Disposition'] = content
+      download = request.GET.get("download")
+      if download:
+        content = "attachment; filename='%s'" %(filename)
+      return response
     return HttpResponse(pdf, content_type='application/pdf')
