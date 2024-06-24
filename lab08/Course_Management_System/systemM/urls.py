34773d9915d82669912eaea2165f5e45001ea4fb
Alsnj20 <marieljara656@gmail.com>
2024-06-23 22:28:11 -0500

    Se creo las vistas que listan a los estudiantes y a los cursos

--- lab08/Course_Management_System/systemM/urls.py
+++ lab08/Course_Management_System/systemM/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register_student/', views.register_student, name='register_student'),
     path('register_course/', views.register_course, name='register_course'),
+    path('list_students/', views.list_students, name='list_students'),
+    path('list_courses/', views.list_courses, name='list_courses'),
+
 ]
--- lab08/Course_Management_System/systemM/views.py
+++ lab08/Course_Management_System/systemM/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Course, Student
 from .forms import CourseForm, StudentForm
 
@@ -11,6 +11,9 @@ def register_student(request):
         if form.is_valid():
             form.save()
             return redirect('index')
+        else:
+          context = {'form': StudentForm()}
+          return render(request, 'register_student.html', context)
     else:
       context = {'form': StudentForm()}
       return render(request, 'register_student.html', context)
@@ -22,6 +25,20 @@ def register_course(request):
         if form.is_valid():
             form.save()
             return redirect('index')
+        else:
+          context = {'form': CourseForm()}
+          return render(request, 'register_course.html', context)
     else:
       context = {'form': CourseForm()}
       return render(request, 'register_course.html', context)
+    
+
+def list_students(request):
+    students = Student.objects.all()
+    context = {'students': students}
+    return render(request, 'list_students.html', context)
+
+def list_courses(request):
+    courses = Course.objects.all()
+    context = {'courses': courses}
+    return render(request, 'list_courses.html', context)
