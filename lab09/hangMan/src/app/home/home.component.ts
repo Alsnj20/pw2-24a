36a1cb94175c71f550b8b7919b2d368fa913f27b
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-07-06 19:41:04 -0500

    implementation of home component

--- lab09/hangMan/src/app/home/home.component.ts
+++ lab09/hangMan/src/app/home/home.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  words: string[] = ['angular', 'typescript', 'component', 'template', 'service'];
+  selectedWord: string = '';
+  displayedWord: string[] = [];
+  incorrectGuesses: number = 0;
+  maxGuesses: number = 6;
+  guessedLetters: string[] = [];
+  gameOver: boolean = false;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.selectedWord = this.words[Math.floor(Math.random() * this.words.length)];
+    this.displayedWord = Array(this.selectedWord.length).fill('_');
+    this.incorrectGuesses = 0;
+    this.guessedLetters = [];
+    this.gameOver = false;
+  }
+
+  guessLetter(letter: string) {
+    if (this.guessedLetters.includes(letter) || this.gameOver) {
+      return;
+    }
+
+    this.guessedLetters.push(letter);
+
+    if (this.selectedWord.includes(letter)) {
+      for (let i = 0; i < this.selectedWord.length; i++) {
+        if (this.selectedWord[i] === letter) {
+          this.displayedWord[i] = letter;
+        }
+      }
+
+      if (!this.displayedWord.includes('_')) {
+        this.gameOver = true;
+      }
+    } else {
+      this.incorrectGuesses++;
+
+      if (this.incorrectGuesses >= this.maxGuesses) {
+        this.gameOver = true;
+      }
+    }
+  }
 }
