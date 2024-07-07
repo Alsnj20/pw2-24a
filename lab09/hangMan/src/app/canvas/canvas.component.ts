ac7f2c78874b90115505eb421998fb0bef093c73
Alsnj20 <marieljara656@gmail.com>
2024-07-06 19:32:01 -0500

    Se agrega los ciclos de vida  afterviewinit y onchange en el componente canvas

--- lab09/hangMan/src/app/canvas/canvas.component.ts
+++ lab09/hangMan/src/app/canvas/canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, Input, AfterViewInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-canvas',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './canvas.component.html',
   styleUrl: './canvas.component.css'
 })
-export class CanvasComponent {
+
+export class CanvasComponent implements AfterViewInit, OnChanges {
+  @ViewChild('itemCanvas', {static: false}) canvas: ElementRef<HTMLCanvasElement> | undefined;
+
+  @Input() step: number = 0;
+  private ctx: CanvasRenderingContext2D | null = null;
+
+
+  ngAfterViewInit(): void {
+    this.ctx = this.canvas ? this.canvas.nativeElement.getContext('2d'): null;
+    this.drawHangman(this.step);
+  }
+
+  ngOnChanges(): void {
+    this.drawHangman(this.step);
+  }
 
 }
